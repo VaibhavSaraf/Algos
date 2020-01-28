83d9628c0cd9194367e77685d955183e0b13ee0b
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2020-01-28 10:02:26 +0530

    modified:   codes/a.out 	modified:   codes/b.cpp

--- codes/b.cpp
+++ codes/b.cpp
@@ -1,8 +1,23 @@
-#include<iostream>
-#include<math.h>
+#include <bits/stdc++.h>
+#include <cstring>
 using namespace std;
 int main()
 {
-    cout<<sqrt(6)<<endl;
+    int arr[6][6];
+    for(int i=0;i<6;i++)
+    {
+        for(int j=0;j<6;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    for(int i=0;i<6;i++)
+    {
+        for(int j=0;i<6;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }

60eed97e189721183a1872e50ec17c4e6c48f92e
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2020-02-10 11:39:13 +0530

    modified:   codes/codechef/longfeb20/A2.cpp 	modified:   codes/codechef/longfeb20/a.out

--- codes/codechef/longfeb20/A2.cpp
+++ codes/codechef/longfeb20/A2.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 using namespace std;
-
-
 int main()
 {
-    
-    
-    
-    
+    int T;
+    cin>>T;
+    while (T)
+    {
+        int n,k;
+        cin>>n>>k;
+        long int *arr = new long int [n];
+        long int sum = 0;
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }   
+        for(int i=0;i<n;i++)
+        {
+            sum = sum + arr[i]%k;
+        }
+        sum = sum % k;
+        cout<<sum<<endl;
+        T--;
+    }
     return 0;
 }

52d410be2b5ca5de48095120fcae72ccc4e100f4
Pratiksha <pratikshasancheti14@gmail.com>
2020-02-19 20:10:06 +0530

    modified:   a.out 	modified:   square.cpp

--- code/square.cpp
+++ code/square.cpp
@@ -1,30 +1,24 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
     int n;
     cin>>n;
-    int count=0;
-    for(int i=0;i<n;i++)
-        cout<<"* ";
-    cout<<endl;
-    int m=n-2;
-    while(m--)
-    {
-    for(int i=0;i<=n;i++)
-    {
-        if(i==0) 
-        {
-            cout<<"* ";
-        }
-        if(i==n)
-            cout<<" *";
-       cout<<" ";
-    }
-    cout<<endl;
-    }
-    for(int i=0;i<n;i++)
-        cout<<"* ";
-    cout<<endl;
+    char a[n][n];
+    memset(a,'*',sizeof(a));
 
+    for(int i=1;i<n-1;i++)
+    {
+        for(int j=1;j<n-1;j++)
+            a[i][j]=' ';
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+            cout<<a[i][j]<<" ";
+
+        cout<<endl;
+    }
+
+    
 }

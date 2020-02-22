8b5a27909240dbd6adf5ed282532305cc14b4964
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2020-02-23 03:57:11 +0530

    modified:   codes/hackerrank/a.out 	modified:   codes/hackerrank/alternatingCharacters.cpp

--- codes/hackerrank/alternatingCharacters.cpp
+++ codes/hackerrank/alternatingCharacters.cpp
@@ -1,23 +1,40 @@
 #include <bits/stdc++.h>
-#define fast ios_base::sync_with_stdio(0); cin.tie(NULL);
+#define fast                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(NULL);
 using namespace std;
 int main()
 {
-    fast
-    int T;
-    cin>>T;
-    int count=0;
+    fast int T;
+    cin >> T;
+    int count = 0;
     string s;
     while (T--)
     {
         count = 0;
-        cin>>s;
+        cin >> s;
+        bool flag = false;
         for(int i=0;i<s.length();i++)
         {
+            if(s[i]!=s[i+1])
+            {
+                flag=false;
+                count++;
+            }
+            else
+            {
+                flag=true;
+            }
             
         }
+        if(flag)
+        {
+            cout<<s.length()-1<<endl;
+        }
+        else
+        {
+            cout<<s.length()-count<<endl;    
+        }       
     }
-    cout<<count<<endl;
-
-
+    return 0;
 }

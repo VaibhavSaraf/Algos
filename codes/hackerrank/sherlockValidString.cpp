6eea49b1830eb9595f8f9794cc4c48db79a49c5c
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2020-03-09 17:15:02 +0530

    modified:   codes/a.out 	modified:   codes/hackerrank/a.out 	modified:   codes/hackerrank/sherlockValidString.cpp

--- codes/hackerrank/sherlockValidString.cpp
+++ codes/hackerrank/sherlockValidString.cpp
@@ -1,29 +1,40 @@
 #include <bits/stdc++.h>
-#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);
+#define fast ios_base::sync_with_stdio(0); cin.tie(NULL);
 using namespace std;
 int main()
 {
     fast
-    string s;
-    bool flag = false;
-    cin>>s;
-    map<char,int> m;
-    for(char x : s)
-    {
-        m[x]++;
-    }
-    auto i = m.begin();
-    int n = i->second;
-    int *arr = new int [s.length()];
-    for(auto x : m)
-    {
-        
-    }
-    if(flag)
-        cout<<"YES"<<endl;
-    else
-        cout<<"NO"<<endl;
+    string s; cin >> s;
+
+    int cnt[26];
     
+
     
+    memset(cnt,0,sizeof(cnt));
+
+    for(int i=0;i<s.length();i++)
+    {
+        cnt[s[i]-'a']++;
+    }
+    
+    for(int i=0;i<26;i++)
+    {
+        if(i>=0 && cnt[i]==0)
+        {
+            continue;
+        }
+
+        if(i>=0)
+        {
+            cnt[i]--;
+        }
+
+        set <int> s;
+
+    }
+
+
+    set<int> s;
+
     return 0;
 }
---
+++ codes/set.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <set>
+#define fast ios_base::sync_with_stdio(0); cin.tie(NULL);
+using namespace std;
+
+int main()
+{
+    
+    
+    
+    return 0;
+}

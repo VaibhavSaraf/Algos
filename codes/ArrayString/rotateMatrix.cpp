e3a2c144c3450610577e811f062b62b44967c714
VaibhavSaraf <vaibhavgsaraf@gmail.com>
2020-03-09 17:44:36 +0530

    modified:   codes/ArrayString/a.out 	new file:   codes/ArrayString/rotateMatrix.cpp

---
+++ codes/ArrayString/rotateMatrix.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define fast sync_with_stdio(0); cin.tie(NULL);
+using namespace std;
+int main()
+{
+    int n,a;cin>>n;
+    
+    int arr[n][n],arr1[n][n];
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+        {
+            cin>>arr[i][j];
+        }
+    
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            arr1[i][j]=arr[n-1-j][i];
+        }
+    }
+    
+    cout<<endl;
+
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<arr1[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
